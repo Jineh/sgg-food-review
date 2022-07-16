ad65204bf6e1d85f7beec103a285ebe657434df8
chaerin <krin0525@gmail.com>
2022-07-17 02:25:58 +0900

    메뉴 리스트 임시 커밋.

--- src/main/java/com/sgg/foodreview/food/repository/FoodRepositoryImpl.java
+++ src/main/java/com/sgg/foodreview/food/repository/FoodRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.sgg.foodreview.food.repository;
 
+import com.sgg.foodreview.food.dto.FoodResponseDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class FoodRepositoryImpl {
