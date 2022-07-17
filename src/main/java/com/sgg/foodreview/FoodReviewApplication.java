9e655bef34648dd7c5d4973f7227a81fd896181c
Jineh <lst5518@naver.com>
2022-07-17 14:52:58 +0900

    mappedSupper

--- src/main/java/com/sgg/foodreview/FoodReviewApplication.java
+++ src/main/java/com/sgg/foodreview/FoodReviewApplication.java
@@ -2,7 +2,14 @@ package com.sgg.foodreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class FoodReviewApplication {
 
@@ -13,6 +20,12 @@ public class FoodReviewApplication {
     public static void main(String[] args) {
         SpringApplication.run(FoodReviewApplication.class, args);
 
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+        //return () -> Optional.of(UUID.randomUUID().toString());
+        return () -> Optional.of("user1");
     }
 
 
